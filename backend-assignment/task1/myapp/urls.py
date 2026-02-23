4c2058f52a54f2593c74c97130c9def19e01e53d
inv-ashwin <ashwin.sanalkumar@innovature.com>
2026-02-23 14:42:46 +0530

    feat(files): implement robust multi-file upload with metadata support
    - Added FileStorageService to handle SHA-256 hashing and size validation.
    - Updated FileUploadView to support multiple file uploads via getlist().
    - Enforced 100MB limit and soft-delete logic for user data integrity.

--- backend-assignment/task1/myapp/urls.py
+++ backend-assignment/task1/myapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from myapp.views import RegisterView
+from myapp.views import RegisterView, FileUploadView, FileListView, FileDetailView, FileDeleteView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns=[
     path('register/',RegisterView.as_view(),name='register'),
     path('login/',TokenObtainPairView.as_view(),name='token_obtain'),
-    path('refresh/',TokenRefreshView.as_view(),name="token_refresh")
+    path('refresh/',TokenRefreshView.as_view(),name="token_refresh"),
+    #FILE CRUD
+    path('upload/', FileUploadView.as_view(), name='file-upload'),
+    path('list/', FileListView.as_view(), name='file-list'),
+    path('detail/<uuid:pk>/', FileDetailView.as_view(), name='file-detail'),
+    path('delete/<uuid:pk>/', FileDeleteView.as_view(), name='file-delete'),
 ]
